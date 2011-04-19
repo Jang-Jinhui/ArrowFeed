a50514671e1931b2bfd6d94ec38a02aa7cc7249e
Jang-Jinhui <dnekfl77@gmail.com>
2011-04-19 19:28:15 +0900

    edit listadapter

--- src/com/protory/arrow/rss/controller/FeedListAdapter.java
+++ src/com/protory/arrow/rss/controller/FeedListAdapter.java
@@ -1,5 +1,15 @@
 package com.protory.arrow.rss.controller;
 
-public class FeedListAdapter {
+import java.util.List;
 
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import com.protory.arrow.rss.domain.Feed;
+
+public class FeedListAdapter extends ArrayAdapter<Feed> {
+
+    public FeedListAdapter(Context context, List<Feed> items) {
+        super(context, android.R.layout.simple_list_item_1, items);
+    }
 }
